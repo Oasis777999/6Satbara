edcd26979836276ffdfa6b4781697c52b826d3b5
Oasis777999 <collegeoasis915@gmail.com>
2025-10-01 18:25:22 +0530

    Updated the ports

--- Frontend/src/api/api.tsx
+++ Frontend/src/api/api.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL:"https://api.satbaraa.com", //VPS ackend
-  baseURL: "http://localhost:5000", //Local Backend
+  baseURL:"https://api.satbaraa.com", //VPS ackend
+  // baseURL: "http://localhost:6000", //Local Backend
   // baseURL: "https://sixsatbara.onrender.com", //Local Backend
 });
 
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 5000;
+const port = 6000;
 
 require("dotenv").config();
 require("./Config/connect");
